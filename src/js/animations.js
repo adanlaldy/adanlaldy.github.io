fcb813471a90151187f7394fcc7cb669cbd51a47
adanlaldy1 <adanlaldy@gmail.com>
2025-01-03 18:17:45 +0100

    feat: add underline on articles hover

--- src/js/animations.js
+++ src/js/animations.js
@@ -1,4 +1,3 @@
-// Sélectionnez toutes les images des liens
 const links = [
     document.getElementById('link1'),
     document.getElementById('link2'),
@@ -12,17 +11,16 @@ const buttons = [
     document.getElementById('button1'),
     document.getElementById('button2.1'),
     document.getElementById('button2.2')
-]
+];
 
-// Ajoutez les événements pour agrandir les images au survol
 links.forEach(link => {
     link.addEventListener('mouseover', () => {
-        link.style.transform = 'scale(1.2)'; // Agrandissement
-        link.style.transition = 'transform 0.3s'; // Animation douce
+        link.style.transform = 'scale(1.2)';
+        link.style.transition = 'transform 0.3s';
     });
 
     link.addEventListener('mouseout', () => {
-        link.style.transform = 'scale(1)'; // Retour à la taille normale
+        link.style.transform = 'scale(1)';
     });
 });
 
@@ -34,5 +32,5 @@ buttons.forEach(button => {
 
     button.addEventListener('mouseout', () => {
         button.style.transform = 'scale(1)';
-    })
+    });
 });
